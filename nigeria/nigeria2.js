38582e802a3442fec56e8c5f90cfd1e0d798db68
Philip Bogden <pbogdude@gmail.com>
2014-04-10 13:30:56 -0400

    tweak

--- nigeria/nigeria2.js
+++ nigeria/nigeria2.js
@@ -16,29 +16,19 @@ var path = d3.geo.path()
     .projection(projection);
 
 // Read the data
-//d3.json("geojson10m.json", function(error, data) {
 d3.json("nigeria.json", function(error, data) {
 
-  json = data; // global for TESTING
-  console.log("# of features: " + data.features.length);
-
-  // Get the features (i.e., states & provinces) for myCountry
-  var myCountry = "Nigeria";
-  admin = data.features.filter( function(d) { return (d.properties.admin == myCountry)?d:false; });
-  console.log("# of features in " + myCountry + ": " + admin.length);
-
-  admin.forEach( function(d,i) {
+  data.forEach( function(d,i) {
      if (d.properties.name == 'Federal Capital Territory') { d.properties.name = 'Abuja' };
-     console.log("admin: " + d.properties.admin + ", " + i + " name: " + d.properties.name);
+     console.log("data: " + d.properties.admin + ", " + i + " name: " + d.properties.name);
      var color = stuff.filter( function(dd){ return (dd.State == d.properties.name ) });
      color = color[0].color;
      console.log("name, color: " + d.properties.name + ", " + color);
      d.color = color;
   });
-  console.log("# of values for which admin == " + myCountry + ": " + admin.length);
 
   svg.selectAll("path")
-      .data(admin)
+      .data(data)
     .enter()
       .append("path")
       .attr("class", "country")
@@ -50,7 +40,7 @@ d3.json("nigeria.json", function(error, data) {
 
   // Label the countries listed in names
   svg.selectAll("text")
-      .data(admin)
+      .data(data)
     .enter()
       .append("text")
       .attr("class", "special-label")
